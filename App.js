1d8f0d2fd4ac691a66a6690974c63daae4dcc6eb
Wolfgang Becker <wolli.becker@gmail.com>
2018-09-01 17:40:25 +0200

    Create History component to fetch calendar data

--- App.js
+++ App.js
@@ -4,17 +4,14 @@ import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import History from "./components/History";
 
 export default class App extends React.Component {
-  state = {
-    value: 0
-  };
-
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
