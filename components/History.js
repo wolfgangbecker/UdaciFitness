49f2f4466ca21261afce3803204d808978bf498b
Wolfgang Becker <wolli.becker@gmail.com>
2018-09-02 11:24:27 +0200

    Create MetricCard for History

--- components/History.js
+++ components/History.js
@@ -13,6 +13,7 @@ import { timeToString, getDailyReminderValue } from "../utils/helpers";
 import UdacifitnessCalendar from "udacifitness-calendar";
 import { white } from "../utils/colors";
 import DateHeader from "./DateHeader";
+import MetricCard from "./MetricCard";
 
 class History extends Component {
   componentDidMount() {
@@ -40,7 +41,7 @@ class History extends Component {
         </View>
       ) : (
         <TouchableOpacity onPress={() => console.log("Pressed!")}>
-          <Text>{JSON.stringify(metrics)}</Text>
+          <MetricCard metrics={metrics} date={formattedDate} />
         </TouchableOpacity>
       )}
     </View>
