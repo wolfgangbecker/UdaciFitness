4dcbbabc3fb28e7f860fce11f6e6ebff513ff0bf
Wolfgang Becker <wolli.becker@gmail.com>
2018-09-05 22:38:13 +0200

    Create Live component with dummy data

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import {
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { purple, white } from "./utils/colors";
 import { Constants } from "expo";
+import Live from "./components/Live";
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -39,6 +40,15 @@ const Tabs = createMaterialTopTabNavigator(
         tabBarLabel: "Add Entry",
         tabBarIcon: ({ tintColor }) => (
           <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    },
+    Live: {
+      screen: Live,
+      navigationOptions: {
+        tabBarLabel: "Live",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-speedometer" size={30} color={tintColor} />
         )
       }
     }
